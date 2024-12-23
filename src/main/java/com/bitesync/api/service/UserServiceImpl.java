2b7a481cf466218784fa47c5fc8d7f3a8f5b2a33
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-12-22 21:18:52 -0700

    Implement BCrypt encoding on user password when signing up

--- src/main/java/com/bitesync/api/service/UserServiceImpl.java
+++ src/main/java/com/bitesync/api/service/UserServiceImpl.java
@@ -3,14 +3,18 @@ package com.bitesync.api.service;
 import com.bitesync.api.entity.User;
 import com.bitesync.api.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
+
     private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public void signupUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 }
