4cc16bf287c95e1f8e2d3b51efee179df365719c
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-12-22 20:47:22 -0700

    Create User repository

--- src/main/java/com/bitesync/api/entity/User.java
+++ src/main/java/com/bitesync/api/entity/User.java
@@ -2,7 +2,6 @@ package com.bitesync.api.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @RequiredArgsConstructor
---
+++ src/main/java/com/bitesync/api/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.bitesync.api.repository;
+
+import com.bitesync.api.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
