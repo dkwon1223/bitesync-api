26b4e65176b87f03aa3ecf4d4c3a0378122b9541
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-12-22 20:53:26 -0700

    Add endpoint for /user/signup in controller

--- src/main/java/com/bitesync/api/controller/UserController.java
+++ src/main/java/com/bitesync/api/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.bitesync.api.controller;
 
+import com.bitesync.api.entity.User;
 import com.bitesync.api.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @AllArgsConstructor
 public class UserController {
+
     private UserService userService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@RequestBody User user) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
