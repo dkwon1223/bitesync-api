eb89dd07020a3886b1e8a4a96e0bcc8180419f80
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-12-22 21:21:53 -0700

    Override attemptAuthentication method in AuthenticationFilter

--- src/main/java/com/bitesync/api/security/filter/AuthenticationFilter.java
+++ src/main/java/com/bitesync/api/security/filter/AuthenticationFilter.java
@@ -1,4 +1,26 @@
 package com.bitesync.api.security.filter;
 
-public class AuthenticationFilter {
+import com.bitesync.api.entity.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            System.out.println(user.getUsername());
+            System.out.println(user.getPassword());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return super.attemptAuthentication(request, response);
+    }
 }
