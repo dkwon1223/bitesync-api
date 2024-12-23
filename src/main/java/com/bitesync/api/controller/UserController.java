16e72c6d0a5fb3f75d3f414ec05aa595a96ddef6
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-12-22 20:50:10 -0700

    Autowire connection between repository, service, and controller layers

---
+++ src/main/java/com/bitesync/api/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.bitesync.api.controller;
+
+import com.bitesync.api.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@AllArgsConstructor
+public class UserController {
+    private UserService userService;
+
+}
--- src/main/java/com/bitesync/api/service/UserServiceImpl.java
+++ src/main/java/com/bitesync/api/service/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.bitesync.api.service;
 
+import com.bitesync.api.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UserServiceImpl implements UserService {
-
+    private UserRepository userRepository;
 }
