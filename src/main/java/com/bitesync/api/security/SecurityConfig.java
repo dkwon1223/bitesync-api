19a8d8c3a2058e76fc11677786cb679c871c5545
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-12-22 20:37:13 -0700

    Configure SecurityConfig

---
+++ src/main/java/com/bitesync/api/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.bitesync.api.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .headers((headers) -> headers
+                        .frameOptions(frameOptions -> frameOptions.disable())
+                )
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/h2/**").permitAll()
+                        .requestMatchers("/user/signup").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
