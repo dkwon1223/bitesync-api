38494fa29c29d9bb9f98c8a0a3675ed5336bf604
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2024-12-22 20:25:48 -0700

    Initial commit

---
+++ pull_request_template.md
@@ -0,0 +1,6 @@
+## Summary
+- Briefly describe the purpose of the PR.
+
+## Changes
+- List key changes or updates made in this PR.
+
---
+++ src/test/java/com/bitesync/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitesync.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
