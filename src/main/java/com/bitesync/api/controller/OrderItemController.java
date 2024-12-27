4901fa31f575bd6d259412179862989337dd1ff4
David Kwon <david.kwon@clover.com>
2024-12-27 11:25:18 -0700

    Implement logic to read all OrderItem and OrderItem by id

--- src/main/java/com/bitesync/api/controller/OrderItemController.java
+++ src/main/java/com/bitesync/api/controller/OrderItemController.java
@@ -1,9 +1,18 @@
 package com.bitesync.api.controller;
 
+import com.bitesync.api.entity.OrderItem;
 import com.bitesync.api.service.OrderItemService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -11,4 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderItemController {
 
   private OrderItemService orderItemService;
+
+  @GetMapping("/all")
+  public ResponseEntity<List<OrderItem>> getAll() {
+    return new ResponseEntity<>(orderItemService.findAllOrderItems(), HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<OrderItem> getById(@PathVariable Long id) {
+    return new ResponseEntity<>(orderItemService.findOrderItemById(id), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<OrderItem> createOrderItem(@RequestBody OrderItem orderItem) {
+    return new ResponseEntity<>(orderItemService.save(orderItem), HttpStatus.CREATED);
+  }
 }
--- src/main/java/com/bitesync/api/service/OrderItemService.java
+++ src/main/java/com/bitesync/api/service/OrderItemService.java
@@ -1,4 +1,11 @@
 package com.bitesync.api.service;
 
+import com.bitesync.api.entity.OrderItem;
+
+import java.util.List;
+
 public interface OrderItemService {
+  OrderItem save(OrderItem orderItem);
+  List<OrderItem> findAllOrderItems();
+  OrderItem findOrderItemById(Long id);
 }
