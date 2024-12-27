e656b5c5632104ddea8585362e7cedae3b115488
David Kwon <david.kwon@clover.com>
2024-12-26 19:38:14 -0700

    Add read by username functionality

--- src/main/java/com/bitesync/api/service/UserServiceImpl.java
+++ src/main/java/com/bitesync/api/service/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package com.bitesync.api.service;
 
 import com.bitesync.api.entity.User;
+import com.bitesync.api.exception.EntityNotFoundException;
 import com.bitesync.api.repository.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
@@ -24,7 +25,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUserByUsername(String username) {
-        return null;
+        Optional<User> user = userRepository.findByUsername(username);
+        return unwrapUser(user, 404L);
     }
 
     static User unwrapUser(Optional<User> entity, Long id) {
