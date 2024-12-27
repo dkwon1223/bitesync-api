610c92350f28b04009a2a6bd1588dc6b36cdc8de
David Kwon <david.kwon@clover.com>
2024-12-27 08:50:31 -0700

    Add MenuItem repository, service, and controller layers

---
+++ src/main/java/com/bitesync/api/controller/MenuItemController.java
@@ -0,0 +1,11 @@
+package com.bitesync.api.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/menu")
+public class MenuItemController {
+}
---
+++ src/main/java/com/bitesync/api/repository/MenuItemRepository.java
@@ -0,0 +1,8 @@
+package com.bitesync.api.repository;
+
+import com.bitesync.api.entity.MenuItem;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MenuItemRepository extends CrudRepository<MenuItem, Long> {
+
+}
