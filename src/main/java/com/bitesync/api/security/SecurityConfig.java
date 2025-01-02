2052d92bb63924db8f956a2a4e101b41ffe360bf
David Kwon <david.kwon@clover.com>
2025-01-02 08:15:09 -0700

    Disable CORS

--- src/main/java/com/bitesync/api/security/SecurityConfig.java
+++ src/main/java/com/bitesync/api/security/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
         authenticationFilter.setFilterProcessesUrl("/user/authenticate");
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
                 .headers((headers) -> headers
                         .frameOptions(frameOptions -> frameOptions.disable())
                 )
